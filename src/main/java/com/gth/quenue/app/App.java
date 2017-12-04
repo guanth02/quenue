2a1dcd01481a5c0d3acee0eb6ef79a9032cadd70
guanth <2859000700@qq.com>
2017-12-04 20:58:48 +0800

    生产者消费者模型代码完成

--- src/main/java/com/gth/quenue/app/App.java
+++ src/main/java/com/gth/quenue/app/App.java
@@ -1,5 +1,22 @@
 package com.gth.quenue.app;
 
-public class App {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import com.gth.quenue.thread.Consumer;
+import com.gth.quenue.thread.Producer;
+import com.gth.quenue.thread.Storage;
+
+public class App {
+    public static void main(String[] args) {
+        Storage storage = new Storage();
+
+        ExecutorService service = Executors.newCachedThreadPool();
+        Producer producer = new Producer("生产者张三", storage);
+
+        Consumer consumer = new Consumer("消费者李四", storage);
+
+        service.submit(producer);
+        service.submit(consumer);
+    }
 }
